924f0ef036fe217891d24937432b4beba28dcaca
omar1024 <omar1024@protonmail.com>
2021-08-21 01:25:32 +0530

    Hashrouter for routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/home";
 import Blog from "./pages/blog/blog";
